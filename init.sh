ece9c47f43cab0200e23b7ae78c90edff8f2da4a
Convexy <shibuya.keigo@gmail.com>
2021-08-08 20:13:29 +0900

    7th commit

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ sudo apt -y update
 sudo apt -y dist-upgrade
 sudo apt -y autoremove
 
-sudo apt -y install python3 python3-pip python-is-python3 sqlite3
+sudo apt -y install python3 python3-pip python3-venv python-is-python3 sqlite3
 
 python -m venv .
 source bin/activate
