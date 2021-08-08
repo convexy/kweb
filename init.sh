4920dfc2ae14d54a79f132249b6c8f9d16a0b559
Convexy <shibuya.keigo@gmail.com>
2021-08-08 19:38:07 +0900

    third commit

--- init.sh
+++ init.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/bash
+sudo apt -y update
+sudo apt -y dist-upgrade
+sudo apt -y autoremove
+
+sudo apt -y install python3 python-is-python3 sqlite3
+
+python -m venv .
+source bin/activate
+python -m pip install -r requirements.txt
