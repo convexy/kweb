660d285362f0ca7c949b3af6ba4187523d5f44b5
Convexy <shibuya.keigo@gmail.com>
2021-08-08 19:59:06 +0900

    5th commit

--- init.sh
+++ init.sh
@@ -3,8 +3,10 @@ sudo apt -y update
 sudo apt -y dist-upgrade
 sudo apt -y autoremove
 
-sudo apt -y install python3 python3-pip python3-venv python-is-python3 sqlite3
+sudo apt -y install python3 python3-pip python-is-python3 sqlite3
 
 python -m venv .
 source bin/activate
 python -m pip install -r requirements.txt
+
+sqlite3 db/main.sqlite3 < ddl.sql
