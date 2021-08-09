38156a90a6b84b4b3cfea6905dcafc1d53eae1fb
Convexy <shibuya.keigo@gmail.com>
2021-08-09 09:52:06 +0900

    8th commit

--- generate.py
+++ generate.py
@@ -10,11 +10,8 @@ try:
         for j in range(40):
             conn.execute(INSERT_T_MAP, (i / 1000.0, j, i * j / 1000 / 40))
     conn.commit()
-    print("committed")
 except:
     conn.rollback()
-    print("rollbacked")
 finally:
     if conn:
         conn.close()
-        print("closed")
