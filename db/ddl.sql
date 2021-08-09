c0e71d4fff43d5e1d44d90a9d74afa40120cc184
Convexy <shibuya.keigo@gmail.com>
2021-08-09 10:14:24 +0900

    9th commit

--- db/ddl.sql
+++ db/ddl.sql
@@ -5,3 +5,12 @@ create table T_MAP (
     , VAL real
     , primary key(CX, CY)
 );
+
+drop table if exists T_MAP2;
+create table T_MAP2 (
+    DT text
+    , Y integer
+    , VAL real
+    , primary key(DT, Y)
+);
+
--- generate.py
+++ generate.py
@@ -1,17 +1,47 @@
 import sqlite3
+import datetime
 
-DELETE_T_MAP = "delete from t_map"
-INSERT_T_MAP = "insert into t_map (CX, CY, VAL) values (?, ?, ?)"
+TARGETS = {
+    "T_MAP": False,
+    "T_MAP2": True,
+}
 
-try:
-    conn = sqlite3.connect("./db/main.sqlite3")
-    conn.execute(DELETE_T_MAP)
-    for i in range(1000):
-        for j in range(40):
-            conn.execute(INSERT_T_MAP, (i / 1000.0, j, i * j / 1000 / 40))
-    conn.commit()
-except:
-    conn.rollback()
-finally:
-    if conn:
-        conn.close()
+if TARGETS["T_MAP"]:
+
+    DELETE_T_MAP = "delete from T_MAP"
+    INSERT_T_MAP = "insert into T_MAP (CX, CY, VAL) values (?, ?, ?)"
+
+    try:
+        conn = sqlite3.connect("./db/main.sqlite3")
+        conn.execute(DELETE_T_MAP)
+        for i in range(1000):
+            for j in range(40):
+                conn.execute(INSERT_T_MAP, (i, j, i * j / 1000 / 40))
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+if TARGETS["T_MAP2"]:
+
+    DELETE_T_MAP2 = "delete from T_MAP2"
+    INSERT_T_MAP2 = "insert into T_MAP2 (DT, Y, VAL) values (?, ?, ?)"
+
+    try:
+        conn = sqlite3.connect("./db/main.sqlite3")
+        conn.execute(DELETE_T_MAP2)
+        dt = datetime.datetime(2021, 7, 1, 0, 0, 0)
+        for i in range(3600):
+            dt = dt + datetime.timedelta(seconds=1)
+            for j in range(40):
+                conn.execute(INSERT_T_MAP2, (dt, j, i * j / 1000 / 40))
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        print(e)
+    finally:
+        if conn:
+            conn.close()
